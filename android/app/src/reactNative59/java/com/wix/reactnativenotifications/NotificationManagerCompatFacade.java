7e2cfd2c4d56d4737fc563203a7efb014c9af290
Bui Quoc Hung <qhu91it@gmail.com>
2019-09-17 16:25:22 +0700

    upgrade androidX

--- android/app/src/reactNative59/java/com/wix/reactnativenotifications/NotificationManagerCompatFacade.java
+++ android/app/src/reactNative59/java/com/wix/reactnativenotifications/NotificationManagerCompatFacade.java
@@ -2,8 +2,9 @@
 package com.wix.reactnativenotifications;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationManagerCompat;
+
+import androidx.core.app.NotificationManagerCompat;
+import androidx.annotation.NonNull;
 
 public abstract class NotificationManagerCompatFacade {
     public static NotificationManagerCompat from(@NonNull Context context) {
